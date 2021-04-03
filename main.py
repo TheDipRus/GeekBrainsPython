dd7ff6d4bd05b75f0ab54edcead0354ebce5ef35
Dmitry <thediprus@gmail.com>
2021-04-03 03:44:34 +0300

    Добавлена функция fn Добавил функцию сложения
    test test test

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ def maths(a,b):
 
 maths(3, 7)
 fn('Test git')
+
+
+def fn(strk):
+    print(strk)
+
+
+def maths(a,b):
+    print(a+b)
+
+    def fn(strk):
+        print(strk)
+
+    def maths(a, b):
+        print(a + b)
+
+        def fn(strk):
+            print(strk)
+
+        def maths(a, b):
+            print(a + b)
+
+            def fn(strk):
+                print(strk)
+
+            def maths(a, b):
+                print(a + b)
